b30eaa42c2759d343de481d63cf537155b2b04f8
Rchunk <rc4548301@gmail.com>
2024-08-15 11:41:16 +0530

    new updation

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+/**
+ * Header
+ *  -Logo
+ *  -Nav items
+ * Body
+ *  -Search
+ *  -Card container
+ *    -Restuaurant card
+ * Footer
+ *  -Copyright
+ *  -Links
+ *  -address
+ *  -Contact
+ */
