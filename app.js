31023f542af76d3b8e4c7aa6beaad81e68b6f1bf
Rchunk <rc4548301@gmail.com>
2024-08-15 15:23:32 +0530

    new updation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import logo from "./images/download.png";
+import "./index.css";
 /**
  * Header
  *  -Logo
@@ -15,3 +16,54 @@ import ReactDOM from "react-dom/client";
  *  -address
  *  -Contact
  */
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img src={logo}></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = () => {
+    return(
+        <div className="res-card">
+            <h3>Hansu chole bhature</h3>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="Search">Search</div>
+            <div className="res-container">
+                <RestaurantCard />
+            </div>
+        </div>
+    )
+}
+
+const Applayout = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Applayout />);
+
+
