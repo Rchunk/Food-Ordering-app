49474c447d3b5c5bfdb0c1d013d1a242b1caead8
Rchunk <rc4548301@gmail.com>
2024-08-16 12:54:57 +0530

    no message

--- app.js
+++ app.js
@@ -62,13 +62,9 @@ const Body = () => {
     <div className="body">
       <div className="Search">Search</div>
       <div className="res-container">
-        <RestaurantCard resData={reslist[0]}  />
-        <RestaurantCard resData={reslist[1]}  />
-        <RestaurantCard resData={reslist[2]}  />
-        <RestaurantCard resData={reslist[0]}  />
-        <RestaurantCard resData={reslist[1]}  />
-        <RestaurantCard resData={reslist[2]}  />
-      
+        {reslist.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
       </div>
     </div>
   );
@@ -76,226 +72,214 @@ const Body = () => {
 
 const reslist = [
   {
-    "info": {
-      "id": "441815",
-      "name": "La Pino'z Pizza",
-      "cloudinaryImageId": "vtv5blaldlnundtxkawl",
-      "locality": "Banwari Market",
-      "areaName": "Rajpura",
-      "costForTwo": "₹300 for two",
-      "cuisines": [
-        "Pizzas",
-        "Pastas",
-        "Italian",
-        "Desserts",
-        "Beverages"
-      ],
-      "avgRating": 4.2,
-      "parentId": "4961",
-      "avgRatingString": "4.2",
-      "totalRatingsString": "500+",
-      "sla": {
-        "deliveryTime": 64,
-        "lastMileTravel": 0.6,
-        "serviceability": "SERVICEABLE",
-        "slaString": "60-65 mins",
-        "lastMileTravelString": "0.6 km",
-        "iconType": "ICON_TYPE_EMPTY"
+    info: {
+      id: "441815",
+      name: "La Pino'z Pizza",
+      cloudinaryImageId: "vtv5blaldlnundtxkawl",
+      locality: "Banwari Market",
+      areaName: "Rajpura",
+      costForTwo: "₹300 for two",
+      cuisines: ["Pizzas", "Pastas", "Italian", "Desserts", "Beverages"],
+      avgRating: 4.2,
+      parentId: "4961",
+      avgRatingString: "4.2",
+      totalRatingsString: "500+",
+      sla: {
+        deliveryTime: 64,
+        lastMileTravel: 0.6,
+        serviceability: "SERVICEABLE",
+        slaString: "60-65 mins",
+        lastMileTravelString: "0.6 km",
+        iconType: "ICON_TYPE_EMPTY",
       },
-      "availability": {
-        "nextCloseTime": "2024-08-16 22:45:00",
-        "opened": true
+      availability: {
+        nextCloseTime: "2024-08-16 22:45:00",
+        opened: true,
       },
-      "badges": {
-        "imageBadges": [
+      badges: {
+        imageBadges: [
           {
-            "imageId": "Rxawards/_CATEGORY-Pizza.png",
-            "description": "Delivery!"
-          }
-        ]
-      },
-      "isOpen": true,
-      "type": "F",
-      "badgesV2": {
-        "entityBadges": {
-          "imageBased": {
-            "badgeObject": [
-              {
-                "attributes": {
-                  "description": "Delivery!",
-                  "imageId": "Rxawards/_CATEGORY-Pizza.png"
-                }
-              }
-            ]
+            imageId: "Rxawards/_CATEGORY-Pizza.png",
+            description: "Delivery!",
           },
-          "textBased": {},
-          "textExtendedBadges": {}
-        }
+        ],
       },
-      "aggregatedDiscountInfoV3": {
-        "header": "50% OFF",
-        "subHeader": "UPTO ₹90"
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Rxawards/_CATEGORY-Pizza.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
       },
-      "orderabilityCommunication": {
-        "title": {},
-        "subTitle": {},
-        "message": {},
-        "customIcon": {}
+      aggregatedDiscountInfoV3: {
+        header: "50% OFF",
+        subHeader: "UPTO ₹90",
       },
-      "differentiatedUi": {
-        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
-        "differentiatedUiMediaDetails": {
-          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
-          "lottie": {},
-          "video": {}
-        }
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
       },
-      "reviewsSummary": {},
-      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
-      "restaurantOfferPresentationInfo": {},
-      "externalRatings": {
-        "aggregatedRating": {
-          "rating": "--"
-        }
-      }
-    }
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+    },
   },
   {
-    "info": {
-      "id": "680972",
-      "name": "Flurry's Cakes & Snacks",
-      "cloudinaryImageId": "fdf03b547368f189a047ce87b1fa789c",
-      "locality": "Durga Mandir",
-      "areaName": "Rajpura",
-      "costForTwo": "₹100 for two",
-      "cuisines": [
-        "Chinese",
-        "South Indian"
-      ],
-      "avgRating": 3.8,
-      "veg": true,
-      "parentId": "406843",
-      "avgRatingString": "3.8",
-      "totalRatingsString": "7",
-      "sla": {
-        "deliveryTime": 41,
-        "lastMileTravel": 1.5,
-        "serviceability": "SERVICEABLE",
-        "slaString": "40-45 mins",
-        "lastMileTravelString": "1.5 km",
-        "iconType": "ICON_TYPE_EMPTY"
+    info: {
+      id: "680972",
+      name: "Flurry's Cakes & Snacks",
+      cloudinaryImageId: "fdf03b547368f189a047ce87b1fa789c",
+      locality: "Durga Mandir",
+      areaName: "Rajpura",
+      costForTwo: "₹100 for two",
+      cuisines: ["Chinese", "South Indian"],
+      avgRating: 3.8,
+      veg: true,
+      parentId: "406843",
+      avgRatingString: "3.8",
+      totalRatingsString: "7",
+      sla: {
+        deliveryTime: 41,
+        lastMileTravel: 1.5,
+        serviceability: "SERVICEABLE",
+        slaString: "40-45 mins",
+        lastMileTravelString: "1.5 km",
+        iconType: "ICON_TYPE_EMPTY",
       },
-      "availability": {
-        "nextCloseTime": "2024-08-16 23:00:00",
-        "opened": true
+      availability: {
+        nextCloseTime: "2024-08-16 23:00:00",
+        opened: true,
       },
-      "badges": {},
-      "isOpen": true,
-      "aggregatedDiscountInfoV2": {},
-      "type": "F",
-      "badgesV2": {
-        "entityBadges": {
-          "imageBased": {},
-          "textBased": {},
-          "textExtendedBadges": {}
-        }
+      badges: {},
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
       },
-      "orderabilityCommunication": {
-        "title": {},
-        "subTitle": {},
-        "message": {},
-        "customIcon": {}
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
       },
-      "differentiatedUi": {
-        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
-        "differentiatedUiMediaDetails": {
-          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
-          "lottie": {},
-          "video": {}
-        }
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
       },
-      "reviewsSummary": {},
-      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
-      "restaurantOfferPresentationInfo": {},
-      "externalRatings": {
-        "aggregatedRating": {
-          "rating": "--"
-        }
-      }
-    }
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+    },
   },
   {
-    "info": {
-      "id": "915804",
-      "name": "THE WOK MASTER",
-      "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/26/1a5012d4-0af4-4426-86ac-8cf1f0833fcb_915804.jpg",
-      "locality": "arya samaj road",
-      "areaName": "Rajpura",
-      "costForTwo": "₹350 for two",
-      "cuisines": [
+    info: {
+      id: "915804",
+      name: "THE WOK MASTER",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/26/1a5012d4-0af4-4426-86ac-8cf1f0833fcb_915804.jpg",
+      locality: "arya samaj road",
+      areaName: "Rajpura",
+      costForTwo: "₹350 for two",
+      cuisines: [
         "Chinese",
         "Italian",
         "sandwich",
         "Salads",
         "Beverages",
         "Momos",
-        "Desserts"
+        "Desserts",
       ],
-      "parentId": "287158",
-      "avgRatingString": "NEW",
-      "sla": {
-        "deliveryTime": 40,
-        "lastMileTravel": 1.1,
-        "serviceability": "SERVICEABLE",
-        "slaString": "35-40 mins",
-        "lastMileTravelString": "1.1 km",
-        "iconType": "ICON_TYPE_EMPTY"
+      parentId: "287158",
+      avgRatingString: "NEW",
+      sla: {
+        deliveryTime: 40,
+        lastMileTravel: 1.1,
+        serviceability: "SERVICEABLE",
+        slaString: "35-40 mins",
+        lastMileTravelString: "1.1 km",
+        iconType: "ICON_TYPE_EMPTY",
       },
-      "availability": {
-        "nextCloseTime": "2024-08-16 22:00:00",
-        "opened": true
+      availability: {
+        nextCloseTime: "2024-08-16 22:00:00",
+        opened: true,
       },
-      "badges": {},
-      "isOpen": true,
-      "aggregatedDiscountInfoV2": {},
-      "type": "F",
-      "badgesV2": {
-        "entityBadges": {
-          "imageBased": {},
-          "textBased": {},
-          "textExtendedBadges": {}
-        }
+      badges: {},
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
       },
-      "orderabilityCommunication": {
-        "title": {},
-        "subTitle": {},
-        "message": {},
-        "customIcon": {}
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
       },
-      "differentiatedUi": {
-        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
-        "differentiatedUiMediaDetails": {
-          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
-          "lottie": {},
-          "video": {}
-        }
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
       },
-      "reviewsSummary": {},
-      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
-      "isNewlyOnboarded": true,
-      "restaurantOfferPresentationInfo": {},
-      "externalRatings": {
-        "aggregatedRating": {
-          "rating": "--"
-        }
-      }
-    }
-  }
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      isNewlyOnboarded: true,
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+    },
+  },
 ];
-
-
-
-
 
 const Applayout = () => {
   return (
