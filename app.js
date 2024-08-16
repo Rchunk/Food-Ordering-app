80f0e6bacf7abc1228cf34eab9cc9d74a9cd004e
Rchunk <rc4548301@gmail.com>
2024-08-16 17:59:26 +0530

    new update

--- app.js
+++ app.js
@@ -60,7 +60,9 @@ const RestaurantCard = (props) => {
 const Body = () => {
   return (
     <div className="body">
-      <div className="Search">Search : <input type = "text"></input> </div>
+      <div className="Search">
+        Search : <input type="text"></input>{" "}
+      </div>
       <div className="res-container">
         {reslist.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
@@ -440,6 +442,421 @@ const reslist = [
           textBased: {},
           textExtendedBadges: {},
         },
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+    },
+  },
+  {
+    info: {
+      id: "475554",
+      name: "Mukhi Sweets",
+      cloudinaryImageId: "d6tzf08khgft6ney0g8e",
+      locality: "Near Durga Mandir",
+      areaName: "Rajpura",
+      costForTwo: "₹200 for two",
+      cuisines: ["Sweets", "Desserts", "Fast Food", "Street Food"],
+      avgRating: 4,
+      veg: true,
+      parentId: "285833",
+      avgRatingString: "4.0",
+      totalRatingsString: "10+",
+      sla: {
+        deliveryTime: 46,
+        lastMileTravel: 1.9,
+        serviceability: "SERVICEABLE",
+        slaString: "45-50 mins",
+        lastMileTravelString: "1.9 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-08-16 20:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "10% OFF",
+        subHeader: "UPTO ₹40",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+    },
+  },
+  {
+    info: {
+      id: "927562",
+      name: "Food Smuggler",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/7/18/36381ac7-c0f2-48ee-b130-b4de33b5d37e_927562.jpg",
+      locality: "Rajpura",
+      areaName: "Rajpura",
+      costForTwo: "₹200 for two",
+      cuisines: ["Fast Food", "Burgers"],
+      veg: true,
+      parentId: "81971",
+      avgRatingString: "NEW",
+      sla: {
+        deliveryTime: 41,
+        lastMileTravel: 1.8,
+        serviceability: "SERVICEABLE",
+        slaString: "40-45 mins",
+        lastMileTravelString: "1.8 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-08-16 23:55:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "FREE ITEM",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      isNewlyOnboarded: true,
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+    },
+  },
+  {
+    info: {
+      id: "105174",
+      name: "Eagle Motel",
+      cloudinaryImageId: "j19oylmobgnb1bc0ihvj",
+      locality: "Rajpura",
+      areaName: "Rajpura Locality",
+      costForTwo: "₹250 for two",
+      cuisines: ["Chinese", "North Indian"],
+      avgRating: 4.7,
+      parentId: "75708",
+      avgRatingString: "4.7",
+      totalRatingsString: "50+",
+      sla: {
+        deliveryTime: 50,
+        lastMileTravel: 2.9,
+        serviceability: "SERVICEABLE",
+        slaString: "50-55 mins",
+        lastMileTravelString: "2.9 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-08-16 23:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+    },
+  },
+  {
+    info: {
+      id: "460176",
+      name: "pizza crackerz",
+      cloudinaryImageId: "qnnrndbxm8n72vacxxse",
+      locality: "Rajpura",
+      areaName: "Ganesh Nagar",
+      costForTwo: "₹200 for two",
+      cuisines: ["Pizzas", "Italian", "Fast Food", "Snacks", "Beverages"],
+      avgRating: 3.6,
+      veg: true,
+      parentId: "276499",
+      avgRatingString: "3.6",
+      totalRatingsString: "10+",
+      sla: {
+        deliveryTime: 45,
+        lastMileTravel: 3.7,
+        serviceability: "SERVICEABLE",
+        slaString: "40-45 mins",
+        lastMileTravelString: "3.7 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-08-16 23:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "50% OFF",
+        subHeader: "UPTO ₹100",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+    },
+  },
+  {
+    info: {
+      id: "899557",
+      name: "Burger bristo",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/5/28/39c74914-2a55-4f92-a4a7-2539619770c9_899557.JPG",
+      locality: "Old Rajpura Road",
+      areaName: "Rajpura",
+      costForTwo: "₹300 for two",
+      cuisines: [
+        "Indian",
+        "Juices",
+        "Grill",
+        "Snacks",
+        "Pizzas",
+        "Italian-American",
+        "Pastas",
+        "Healthy Food",
+        "Burgers",
+      ],
+      avgRating: 4.9,
+      veg: true,
+      parentId: "522952",
+      avgRatingString: "4.9",
+      totalRatingsString: "50+",
+      sla: {
+        deliveryTime: 45,
+        lastMileTravel: 2.7,
+        serviceability: "SERVICEABLE",
+        slaString: "45-50 mins",
+        lastMileTravelString: "2.7 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-08-16 23:59:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹39",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      isNewlyOnboarded: true,
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+    },
+  },
+  {
+    info: {
+      id: "458262",
+      name: "Pizza I Land",
+      cloudinaryImageId: "egszynuqjgqaqeoorktv",
+      locality: "Dhakansu Road",
+      areaName: "Rajpura Locality",
+      costForTwo: "₹300 for two",
+      cuisines: ["Pizzas", "Italian", "Beverages", "Burgers", "Snacks"],
+      avgRating: 4.2,
+      veg: true,
+      parentId: "275948",
+      avgRatingString: "4.2",
+      totalRatingsString: "100+",
+      sla: {
+        deliveryTime: 47,
+        lastMileTravel: 4,
+        serviceability: "SERVICEABLE",
+        slaString: "45-50 mins",
+        lastMileTravelString: "4.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-08-16 23:59:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "50% OFF",
+        subHeader: "UPTO ₹100",
       },
       orderabilityCommunication: {
         title: {},
